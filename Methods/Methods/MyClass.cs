21bac71fbe1d6ccdbb09439d4d8de1c92257a3a6
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-06-16 15:47:29 -0400

    methods assignment

---
+++ Methods/Methods/MyClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    public class MyClass
+    {
+        public static int MathAdd(int userNumber)
+        {
+            int numberOne = 2;
+            int resultAdd = numberOne + userNumber;
+            return resultAdd;
+        }
+
+        public static int MathMultiply(int userNumber)
+        {
+            int numberOne = 2;
+            int resultMultiply = numberOne * userNumber;
+            return resultMultiply;
+        }
+
+        public static int MathDivide(int userNumber)
+        {
+            int numberOne = 2;
+            int resultDivide = userNumber / numberOne;
+            return resultDivide;
+        }
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a number you would like to perform math operations with.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+            int resultAdd = MyClass.MathAdd(userNumber);
+            Console.WriteLine("2" + " plus " + userNumber + " equals " + resultAdd);
+            int resultMultiply = MyClass.MathMultiply(userNumber);
+            Console.WriteLine("2" + " times " + userNumber + " equals " + resultMultiply);
+            int resultDivide = MyClass.MathDivide(userNumber);
+            Console.WriteLine(userNumber + " divided by " + "2" + " equals " + resultDivide);
+
+            Console.ReadLine();
+        }
+    }
+}
