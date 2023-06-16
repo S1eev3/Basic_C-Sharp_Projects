351e04bea446ae1a7be448f688a999a73cbc1446
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-06-16 18:25:02 -0400

    Void method assignment

---
+++ VoidMethod/VoidMethod/MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class MyClass
+    {
+        public void NumMath(int numOne, int numTwo)
+        {
+            int numThree = 3;
+            int result = numOne + numThree;
+            Console.WriteLine(numTwo);
+            return;
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass M1 = new MyClass();
+            int numOne = 1;
+            int numTwo = 2;
+            M1.NumMath(numOne, numTwo);
+            M1.NumMath(numOne: 1, numTwo: 2);
+            Console.ReadLine();
+        }
+    }
+}
