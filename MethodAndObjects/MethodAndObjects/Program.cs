437d463b0a2174787c9880079256abacca1b0921
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-06-19 19:56:49 -0400

    Method and Objects assignment

---
+++ MethodAndObjects/MethodAndObjects/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee();
+            Emp1.FirstName = "Sample";
+            Emp1.LastName = "Student";
+            Emp1.SayName();
+            Console.ReadLine();
+        }
+    }
+}
