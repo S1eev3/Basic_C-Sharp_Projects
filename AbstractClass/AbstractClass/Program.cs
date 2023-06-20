5d2fa054ce2ea12bd11af3ad90d4854fdbfe4e74
Mitchal Larson <76798028+S1eev3@users.noreply.github.com>
2023-06-19 20:33:48 -0400

    Abstract class assignment

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee();
+            Emp1.firstName = "Sample";
+            Emp1.lastName = "Student";
+            Emp1.SayName();
+            Console.ReadLine();
+        }
+    }
+}
